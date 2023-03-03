12dd06aa033a7273500f257e1a9541758f1e14ae
5218664b <18387474+5218664b@users.noreply.github.com>
2023-03-03 12:25:13 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -49,8 +49,9 @@ if [ -d "/var/lib/zerotier-one/moons.d" ] # check if the moons conf has generate
 then
         moon_id=$(cat /var/lib/zerotier-one/identity.public | cut -d ':' -f1)
         echo -e "Your ZeroTier moon id is \033[0;31m$moon_id\033[0m, you could orbit moon using \033[0;31m\"zerotier-cli orbit $moon_id $moon_id\"\033[0m"
-        /usr/sbin/zerotier-cli listmoons
+	echo -e "stableEndpointsForSed:$stableEndpointsForSed"
 	/usr/sbin/zerotier-one
+	/usr/sbin/zerotier-cli listmoons
 else
         nohup /usr/sbin/zerotier-one >/dev/null 2>&1 &
         # Waiting for identity generation...'
@@ -65,8 +66,9 @@ else
         pkill zerotier-one
         moon_id=$(cat /var/lib/zerotier-one/moon.json | grep \"id\" | cut -d '"' -f4)
         echo -e "Your ZeroTier moon id is \033[0;31m$moon_id\033[0m, you could orbit moon using \033[0;31m\"zerotier-cli orbit $moon_id $moon_id\"\033[0m"
-        /usr/sbin/zerotier-cli listmoons
+	echo -e "stableEndpointsForSed:$stableEndpointsForSed"
 	exec /usr/sbin/zerotier-one
+	/usr/sbin/zerotier-cli listmoons
 fi
 
 
